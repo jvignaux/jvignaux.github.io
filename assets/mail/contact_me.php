b30f768e7f249b2eba6e70bd876cdb3465fa3597
julie <j.daprato-externe@nicecotedazur.org>
2020-12-03 08:39:51 +0100

    update sprites+mail

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,5 +1,4 @@
 <?php
-// Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
@@ -8,13 +7,15 @@ if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$message = str_replace("\n.", "\n..", $message);
+$message = wordwrap(strip_tags(htmlspecialchars($_POST['message']), 70, "\r\n"););
+
 
 // Create the email and send the message
-$to = "yourname@yourdomain.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "julie.vignaux.pro@gmail.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$body = "Vous avez reçu un message du site julie.vignaux.github.io.\n\n"."Voici les informations:\n\nNom: $name\n\nEmail: $email\n\nTéléphone: $phone\n\nMessage:\n$message";
+$header = "From: noreply@julievignaux.com\n"; // .
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
